ea105c2f57614ac8c4751cd378854fe357e6ca94
unknown <rinovethamoses97@gmail.com>
2020-03-18 18:36:00 +0530

    Title icon added

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(function(req, res, next) {
     res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     next();
 });
+app.use('/favicon.ico', express.static('./public/images/favicon.ico'));
 let mongoose=require("mongoose");
 let Enquiry=require("./models/enquiry");
 let User=require("./models/user");
